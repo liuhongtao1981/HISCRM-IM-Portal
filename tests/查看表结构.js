14839cca81c9906f166e9241e5d75e0d905a8955
立昆 苏 <sulk_clinical@hotmail.com>
2025-11-08 13:30:06 +0800

    移除tests目录的git追踪
    - 将tests/目录加入.gitignore
    - 从git仓库中删除所有测试脚本
    - 保留本地tests目录文件(仅移除追踪)
    
    原因:
    - 大部分测试脚本为临时调试用
    - 无需纳入版本控制
    - 减小仓库体积

--- tests/查看表结构.js
+++
@@ -1,8 +0,0 @@
-const Database = require('better-sqlite3');
-const db = new Database('./packages/master/data/master.db');
-
-console.log('worker_runtime 表结�?');
-const columns = db.prepare("PRAGMA table_info(worker_runtime)").all();
-console.table(columns);
-
-db.close();
